4926afc230c01ea8f22d86a34cb608ed76ad1823
CKTK203 <105462082+CKTK203@users.noreply.github.com>
2024-01-09 17:57:15 -0300

    Update Program.cs
    Update 2

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia("84219", "G60", "517543772443680", 128);
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
+nokia.ReceberLigacao();
 
 Console.WriteLine("Smartphone Iphone:");
 Smartphone iphone = new Iphone("98324", "Iphone 15", "493311803390546", 512);
 iphone.Ligar();
 iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
