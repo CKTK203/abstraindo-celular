12bfc8fc9be98174664bafc843d55eb0f54923cc
CKTK203 <105462082+CKTK203@users.noreply.github.com>
2024-01-09 17:53:20 -0300

    Update Program.cs
    Update 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia("84219", "G60", "517543772443680", 128);
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
 
+Console.WriteLine("Smartphone Iphone:");
 Smartphone iphone = new Iphone("98324", "Iphone 15", "493311803390546", 512);
 iphone.Ligar();
 iphone.InstalarAplicativo("Telegram");
